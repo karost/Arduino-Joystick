0269083d1f5a390a8eff1f3b4abea747569b77e5
Thanit Kebsiri <thanit.k9@gmail.com>
2017-12-28 21:12:33 -0800

    update brake range

--- Arduino/ANALOG_EBRAKE/ANALOG_EBRAKE.ino
+++ Arduino/ANALOG_EBRAKE/ANALOG_EBRAKE.ino
@@ -1,23 +1,23 @@
 // Analog eBrake-Handbrake
 // PIN A0 to 10k pot output - Throttle Axis
-// Use with Arduino ProMicro.
-// 
+// Use with Arduino ProMicro or Sparkfun Pro Micro ATmega32U4 5V/16MHZ
+// Arduino 1.8.3 with Sparkfun Pro Micro driver
 // by THANIT KEBSIRI
 // 28.12.2017
 
 #include "Joystick.h"
 
 //--------------------------------------------------------------------
-//--- CREATE JOYSTICK OBJECT ONLY HAS BRAKE ENABLE
+//--- CREATE JOYSTICK OBJECT AND USE ONLY BRAKE CONTROL
 //--------------------------------------------------------------------
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
 0, 0, // Button Count, Hat Switch Count
-false, false, false, // No X ,Y, Z Axis (Joystick)
-false, false, false, // No Rx, Ry, or Rz
-false, false, // No rudder or throttle
-false, true, false); // No accelerator, add new  brake,  No steering
+false, false, false,  // No X ,Y, Z Axis (Joystick)
+false, false, false,  // No Rx, Ry, or Rz
+false, false,         // No rudder or throttle
+false, true, false);  // No accelerator, add new  brake,  No steering
 
-const bool DEBUG = false; // set to true to debug the raw values
+const bool DEBUG = true; // set to true to debug the raw values
 const int zPin = A0;   // use pin A0
 
 int zCur,zMap ;
@@ -25,7 +25,7 @@ int zMinBrake, zMaxBrake;
 
 void setup(){
   zMinBrake = 0; 
-  zMaxBrake = 1023; 
+  zMaxBrake = 1021; 
 
   pinMode(zPin, INPUT);
   Joystick.setBrakeRange(zMinBrake, zMaxBrake);
